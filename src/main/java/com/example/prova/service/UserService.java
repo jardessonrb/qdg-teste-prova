9e785f7a4d1050c767232b951caf5b8fb44a677d
jardessonrb <jardessonrs1117@gmail.com>
2025-09-03 22:50:06 -0300

    feat: criação do método de listagem de User

--- src/main/java/com/example/prova/service/UserService.java
+++ src/main/java/com/example/prova/service/UserService.java
@@ -1,4 +1,10 @@
 package com.example.prova.service;
 
+import com.example.prova.dto.UserCreatedResponse;
+import com.example.prova.dto.UserFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface UserService {
+    Page<UserCreatedResponse> pagedUserSearch(UserFilter filter, Pageable pagination);
 }
--- src/main/java/com/example/prova/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/prova/service/impl/UserServiceImpl.java
@@ -1,10 +1,37 @@
 package com.example.prova.service.impl;
 
+import com.example.prova.dto.UserCreatedResponse;
+import com.example.prova.dto.UserFilter;
+import com.example.prova.entity.User;
+import com.example.prova.repository.user.UserRepository;
+import com.example.prova.repository.user.UserSpecification;
+import com.example.prova.repository.user.UserSpecificationBuilder;
 import com.example.prova.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Page<UserCreatedResponse> pagedUserSearch(UserFilter filter, Pageable pagination) {
+        Specification<User> specification = UserSpecificationBuilder
+                .builder()
+                .and(UserSpecification.filterByNameContainsIgnoreCase(filter.name()))
+                .and(UserSpecification.filterByPhoneContainsNotMask(Objects.nonNull(filter.phone()) ? filter.phone().replaceAll("\\D", "") : null))
+                .and(UserSpecification.filterByCPFContainsNotMask(Objects.nonNull(filter.cpf()) ? filter.cpf().replaceAll("\\D", "") : null))
+                .build();
+
+        Page<User> usersPage = userRepository.findAll(specification, pagination);
+
+        return usersPage.map(UserCreatedResponse::new);
+    }
 }
