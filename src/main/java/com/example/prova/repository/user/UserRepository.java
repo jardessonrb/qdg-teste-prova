d01ef14b1fb26e45f1916acc8fa21810f069262e
jardessonrb <jardessonrs1117@gmail.com>
2025-09-04 08:18:53 -0300

    feat: Adicionado método para verificar existência de User já com um CPF

--- src/main/java/com/example/prova/repository/user/UserRepository.java
+++ src/main/java/com/example/prova/repository/user/UserRepository.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Boolean existsByCpf(String cpf);
 }
