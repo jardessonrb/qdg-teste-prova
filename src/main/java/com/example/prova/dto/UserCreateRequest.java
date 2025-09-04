6ae714240b93b3be778504c4bfecac6a741078eb
jardessonrb <jardessonrs1117@gmail.com>
2025-09-04 08:17:33 -0300

    feat: adicionado @Builder do lombok.

--- src/main/java/com/example/prova/dto/UserCreateRequest.java
+++ src/main/java/com/example/prova/dto/UserCreateRequest.java
@@ -2,8 +2,10 @@ package com.example.prova.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import org.hibernate.validator.constraints.br.CPF;
 
+@Builder
 public record UserCreateRequest(
     @NotNull(message = "name is required")
     @NotBlank(message = "name cannot be empty")
